540f67bdefda171bcf35e96f3a3dd8195b7e8f44
covy <junni802@naver.com>
2025-08-12 18:35:07 +0900

    feat: add single-thread-coupon-system

---
+++ src/main/java/covy/singlethreadcouponsytstem/SingleThreadCouponSytstemApplication.java
@@ -0,0 +1,13 @@
+package covy.singlethreadcouponsytstem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleThreadCouponSytstemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SingleThreadCouponSytstemApplication.class, args);
+  }
+
+}
---
+++ src/test/java/covy/singlethreadcouponsytstem/SingleThreadCouponSytstemApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.singlethreadcouponsytstem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleThreadCouponSytstemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
